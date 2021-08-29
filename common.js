3ad5e53c02534edd259a2877bb2f9c1b7fe7438d
Andrew <43142383+eralinkd@users.noreply.github.com>
2021-08-29 12:17:38 +0300

    Add files via upload

--- common.js
+++ common.js
@@ -1,25 +1,72 @@
 let nums = document.querySelectorAll('.num');
 let text = document.querySelector('.text-cont');
 let arithmetics = document.querySelectorAll('.arithmetic');
-let isArithmeticsEntered = false;
+let equals = document.querySelector('.equals');
+let arithmeticsEntered = 0;
+let firstNum = 0;
+let a = '';
+let b = '';
+let secondNum = 0;
 
 for (let num of nums){
 	num.onclick = function(){
-		if (text.textContent === '0'){
-			text.textContent = num.textContent;
+		if (arithmeticsEntered === 0){
+			if (a === ''){
+				text.textContent = num.textContent;
+				a = text.textContent;
+				if (num.textContent === '0' || num.textContent === '00'){
+					a = '';
+					text.textContent = '0';
+				}
+
+				firstNum = parseFloat(a);
+				console.log('firstNum = '+firstNum);
+			} else{
+				text.textContent += num.textContent;
+				a = text.textContent;
+				firstNum = parseFloat(a);
+				console.log('firstNum = '+firstNum);
+			}
 		} else{
 			text.textContent += num.textContent;
+			b += num.textContent;
+			secondNum = parseFloat(b);
+			console.log('secondNum = '+secondNum);
 		}
 	}
 }
 
 for (let arithmetic of arithmetics){
 	arithmetic.onclick = function(){
-		if (isArithmeticsEntered === false){
+		if (arithmeticsEntered === 0){
 			text.textContent += arithmetic.textContent;
-			isArithmeticsEntered = true;
+			if (arithmetic.textContent === '/'){
+				arithmeticsEntered = 1;
+			} else if(arithmetic.textContent === '*'){
+				arithmeticsEntered = 2;
+			} else if(arithmetic.textContent === '-'){
+				arithmeticsEntered = 3;
+			} else if(arithmetic.textContent === '+'){
+				arithmeticsEntered = 4;
+			}
 		}
 	}
 }
 
-
+equals.onclick = function(){
+	if (arithmeticsEntered === 1){
+		text.textContent = firstNum / secondNum;
+	} else if(arithmeticsEntered === 2){
+		text.textContent = firstNum * secondNum;
+	} else if(arithmeticsEntered === 3){
+		text.textContent = firstNum - secondNum;
+	} else if(arithmeticsEntered === 4){
+		text.textContent = firstNum + secondNum;
+	}
+	
+	arithmeticsEntered = 0;
+	a = text.textContent;
+	firstNum = parseFloat(a);
+	b = '';
+	secondNum = 0;
+}
